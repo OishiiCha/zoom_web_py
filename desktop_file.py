43a3d78300ac17847caa1a1cb14ca268e52b282f
OishiiCha <86476845+OishiiCha@users.noreply.github.com>
2021-09-19 12:08:12 +0100

    Update desktop_file.py

--- desktop_file.py
+++ desktop_file.py
@@ -4,7 +4,7 @@ import os
 f = open("Zoom_Online.desktop", "x")
 
 usr = str(getpass.getuser())
-f.write("[Desktop Entry]\nVersion=1.0\nName=BackMeUp\nComment=Opens Zoom Web Client\nExec=/home/"+ usr +"/zoom_online/zoom_online.py\nIcon=/home/"+ usr +"/Pictures/zoom.ico\nTerminal=false\nType=Application\nCategories=Utility;Application;")
+f.write("[Desktop Entry]\nVersion=1.0\nName=BackMeUp\nComment=Opens Zoom Web Client\nExec=/home/"+ usr +"/zoom_online_py/zoom_online.py\nIcon=/home/"+ usr +"/zoom_online_py/zoom.ico\nTerminal=false\nType=Application\nCategories=Utility;Application;")
 
-os.system("mv ~/zoom_online/Zoom_Online.desktop ~/Desktop/")
+os.system("mv ~/zoom_online_py/Zoom_Online.desktop ~/Desktop/")
 os.system("chmod +x ~/Desktop/Zoom_Online.desktop")
