65469eff66c011b5384492d812e909d72aa81b55
OishiiCha <86476845+OishiiCha@users.noreply.github.com>
2021-09-19 14:44:55 +0100

    Update desktop_file.py

--- desktop_file.py
+++ desktop_file.py
@@ -1,10 +1,13 @@
 import getpass
 import os
 
-f = open("Zoom_Online.desktop", "x")
+f = open("Zoom_Web.desktop", "x")
 
 usr = str(getpass.getuser())
-f.write("[Desktop Entry]\nVersion=1.0\nName=BackMeUp\nComment=Opens Zoom Web Client\nExec=/home/"+ usr +"/zoom_online_py/zoom_online.py\nIcon=/home/"+ usr +"/zoom_online_py/zoom.ico\nTerminal=false\nType=Application\nCategories=Utility;Application;")
+f.write("[Desktop Entry]\nVersion=1.0\nName=Zoom Web\nComment=Opens Zoom Web Client\nExec=python3 /home/"+ usr +"/zoom_web_py/zoom_web.py\nIcon=/home/"+ usr +"/zoom_web_py/zoom.ico\nTerminal=false\nType=Application\nCategories=Application;")
 
-os.system("mv ~/zoom_online_py/Zoom_Online.desktop ~/Desktop/")
-os.system("chmod +x ~/Desktop/Zoom_Online.desktop")
+
+os.system("mv Zoom_Web.desktop ~/Desktop/")
+os.system("cd")
+os.system("chmod +x ~/Desktop/Zoom_Web.desktop")
+os.system("gio set ~/Desktop/Zoom_Web.desktop metadata::trusted true")
