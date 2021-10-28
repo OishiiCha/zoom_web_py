92605d13386383dcb17379588decd9dd9f9f791f
OishiiCha <86476845+OishiiCha@users.noreply.github.com>
2021-10-28 21:42:19 +0100

    updates

--- zoom_web.py
+++ zoom_web.py
@@ -1,46 +1,69 @@
 from tkinter import *
-import time
 import webbrowser
+import platform
+import csv
+import os
+
+bgc = "grey12"
+
+height = 200
+width = 300
+
+myfont = ("arial",10)
 
 root = Tk()
-root.geometry("300x200")
+root.geometry(str(width)+"x"+str(height))
 root.title("Zoom Web")
+root.config(bg=bgc)
 root.resizable(width=False, height=False)
+if platform.system() == "Windows":
+    root.iconbitmap("data/zoom_web.ico")
 
-
+row1 = int(height/5*1)
+row2 = int(height/5*2)
+row3 = int(height/5*3)
+row4 = int(height/5*4)
 
 def enter_data():
     txt_name = ent.get()
     meeting_id = txt_name.replace(" ", "")
     if len(meeting_id) in (10,11):
         link = "https://pwa.zoom.us/wc/join/" + meeting_id
-        error_message = Label(root, text="                                                     ")
-        error_message.grid(row=5, columnspan=2)
-        time.sleep(1)
+        error_message = Label(root, text="                                                     ", bg=bgc, font=myfont, justify="center")
+        error_message.place(x=int(width/2), y=row3, anchor="center")
         webbrowser.open(link, 1)
     else:
-        error_message = Label(root, text="Please enter a valid meeting ID")
-        error_message.grid(row=5, columnspan=2)
+        error_message = Label(root, text="Please enter a valid meeting ID", bg=bgc, fg="white", font=myfont, justify="center")
+        error_message.place(x=int(width/2), y=row3, anchor="center")
 
 
-spacer = Label(root, text = "")
-spacer2 = Label(root, text = "")
-spacer3 = Label(root, text = "")
-m_id = Label(root, text = "     Meeting ID:    ")
+def set_mem1():
+    if os.path.isfile("data/memory/mem1.txt"):
+        os.remove("data/memory/mem1.txt")
+    with open("data/memory/mem1.txt", "x") as f1:
+
+        f1.writelines(str(ent.get()))
+        f1.close
 
 
-ent = Entry(root, justify = "center")
-
-m_id.grid(row=1)
-spacer.grid(row=0)
-spacer2.grid(row=2)
-spacer3.grid(row=4)
+def read_mem1():
+    f1r = open("data/memory/mem1.txt","r")
+    mem1_data = f1r.read()
+    webbrowser.open("https://pwa.zoom.us/wc/join/"+mem1_data, 1)
 
 
-ent.grid(row=1, column=1)
 
-btn = Button(root, text="Enter", bg="grey", fg="white", command=enter_data)
-btn.grid(row=3, columnspan=2)
+m_id = Label(root, text = "Meeting ID:", bg=bgc, fg="white", font=myfont)
+m_id.place(x=int(width/5*1), y=row1, anchor="center")
+ent = Entry(root, justify="center", bg="white", fg="black", font=myfont)
+ent.place(x=int(width/5*3), y=row1, anchor="center")
+btn = Button(root, text="Enter", bg=bgc, fg="white", font=myfont, command=enter_data)
+btn.place(x=int(width/2), y=row2, anchor="center")
+saved_b = Button(root, text="Open Mem 1", bg="deepskyblue2", fg="white", font=myfont, command= read_mem1)
+saved_b.place(x=int(width/4*1), y=row4, anchor="center")
+set_b = Button(root, text="Save Mem 1", bg="deepskyblue2", fg="white", font=myfont, command=set_mem1)
+set_b.place(x=int(width/4*3), y=row4, anchor="center")
 
+if __name__ == '__main__':
+    root.mainloop()
 
-root.mainloop()
