d43baf34df3f6cb491a902a5e0197bb058dd3046
OishiiCha <86476845+OishiiCha@users.noreply.github.com>
2021-10-31 09:45:19 +0000

    updates
    added 4 memory files

--- zoom_web.py
+++ zoom_web.py
@@ -38,18 +38,19 @@ def enter_data():
 
 
 def set_mem1():
-    if os.path.isfile("data/memory/mem1.txt"):
-        os.remove("data/memory/mem1.txt")
-    with open("data/memory/mem1.txt", "x") as f1:
-
+    data_location = str("data/memory/"+variable.get()+".txt")
+    if os.path.isfile(data_location):
+        os.remove(data_location)
+    with open(data_location, "x") as f1:
         f1.writelines(str(ent.get()))
         f1.close
 
 
 def read_mem1():
-    f1r = open("data/memory/mem1.txt","r")
-    mem1_data = f1r.read()
-    webbrowser.open("https://pwa.zoom.us/wc/join/"+mem1_data, 1)
+    data_location = str("data/memory/"+variable.get()+".txt")
+    f1r = open(data_location,"r")
+    mem_data = f1r.read()
+    webbrowser.open("https://pwa.zoom.us/wc/join/"+mem_data, 1)
 
 
 
@@ -59,9 +60,14 @@ ent = Entry(root, justify="center", bg="white", fg="black", font=myfont)
 ent.place(x=int(width/5*3), y=row1, anchor="center")
 btn = Button(root, text="Enter", bg=bgc, fg="white", font=myfont, command=enter_data)
 btn.place(x=int(width/2), y=row2, anchor="center")
-saved_b = Button(root, text="Open Mem 1", bg="deepskyblue2", fg="white", font=myfont, command= read_mem1)
+saved_b = Button(root, text="Open", bg="deepskyblue2", fg="white", font=myfont, command= read_mem1)
 saved_b.place(x=int(width/4*1), y=row4, anchor="center")
-set_b = Button(root, text="Save Mem 1", bg="deepskyblue2", fg="white", font=myfont, command=set_mem1)
+myoption = ["Mem1", "Mem2", "Mem3", "Mem4"]
+variable = StringVar(root)
+variable.set(myoption[0])
+memoption = OptionMenu(root, variable, *myoption)
+memoption.place(x=int(width/2), y=row4, anchor="center")
+set_b = Button(root, text="Save", bg="deepskyblue2", fg="white", font=myfont, command=set_mem1)
 set_b.place(x=int(width/4*3), y=row4, anchor="center")
 
 if __name__ == '__main__':
